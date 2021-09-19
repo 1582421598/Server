ece2dda7fa48429d1af8b1ab05594441209dfbcd
1582421598 <1582421598@qq.com>
2021-09-19 17:54:59 +0800

    add weighting system for multi song source

--- src/provider/select.js
+++ src/provider/select.js
@@ -1,70 +1,83 @@
-const similarity = require('../similarity')
-
 const ENABLE_WEIGHTINGSYSTEM = (process.env.ENABLE_WEIGHTINGSYSTEM || '').toLowerCase() === 'true'
-
 const replaceSpace = (string) => string.replace(/&nbsp;/g, ' ').replace(/nbsp;/g, ' ')
 
+const compareTwoStrings = (first, second) => {
+	first = first.replace(/\s+/g, '').toLowerCase()
+	second = second.replace(/\s+/g, '').toLowerCase()
+
+	if (first === second) return 1; // identical or empty
+	if (first.length < 2 || second.length < 2) return 0; // if either is a 0-letter or 1-letter string
+
+	let firstBigrams = new Map();
+	for (let i = 0; i < first.length - 1; i++) {
+		const bigram = first.substring(i, i + 2);
+		const count = firstBigrams.has(bigram) ?
+			firstBigrams.get(bigram) + 1 :
+			1;
+
+		firstBigrams.set(bigram, count);
+	};
+
+	let intersectionSize = 0;
+	for (let i = 0; i < second.length - 1; i++) {
+		const bigram = second.substring(i, i + 2);
+		const count = firstBigrams.has(bigram) ?
+			firstBigrams.get(bigram) :
+			0;
+
+		if (count > 0) {
+			firstBigrams.set(bigram, count - 1);
+			intersectionSize++;
+		}
+	}
+
+	return (2.0 * intersectionSize) / (first.length + second.length - 2);
+}
+
 const calcWeight = (song, info) => {
-	var weight = 0
+	let weight = 0
 	const songName = replaceSpace(song.name.replace(/（\s*cover[:：\s][^）]+）/i, '')
 		.replace(/\(\s*cover[:：\s][^\)]+\)/i, '')
 		.replace(/（\s*翻自[:：\s][^）]+）/, '')
-		.replace(/\(\s*翻自[:：\s][^\)]+\)/, '')).toLowerCase()
-	const similarityVaule = similarity.compareTwoStrings(songName, info.name)
-	if (similarityVaule === 0) return 0 //歌曲名不相似绝对不一样    
-	if (similarityVaule === 1) weight = 0.15
-	else weight = similarityVaule / 4
+		.replace(/\(\s*翻自[:：\s][^\)]+\)/, ''))
+	const similarityValue = compareTwoStrings(songName, info.name)
+	if (similarityValue === 0) return 0 //歌曲名不相似绝对不一样    
+	if (similarityValue === 1) weight = 0.15
+	else weight = similarityValue / 4
 
 	if (song.artists) {
-		var authorName = ''
+		let authorName = ''
 		if (Array.isArray(song.artists)) {
 			song.artists.forEach(artists => {
-				authorName = authorName + artists.name.replace(/&nbsp;/g, ' ')
+				authorName += replaceSpace(artists.name)
 			});
 		} else {
-			authorName = song.artists.name
+			authorName = replaceSpace(song.artists.name)
 		}
-		authorName = replaceSpace(authorName).toLowerCase()
 		const songName = song.name ? song.name : ''
 		info.artists.forEach(artists => {
-			const originalName = artists.name.toLowerCase()
-			if (authorName.includes(originalName)) weight = weight + 0.1
-			else if (songName.includes(originalName)) weight = weight + 0.1
-			else weight = weight - 0.1
+			const originalName = artists.name
+			if (authorName.includes(originalName)) weight += 0.1
+			else if (songName.includes(originalName)) weight += 0.1
+			else weight -= 0.1
 		})
 	}
 	if (song.duration) {
 		const songLength = Math.abs(song.duration - info.duration)
-		if (songLength < 3 * 1e3) weight = weight + 0.1
-		else if (songLength < 6 * 1e3) weight = weight + 0.06
-		else if (songLength < 9 * 1e3) weight = weight + 0.03
+		if (songLength < 3 * 1e3) weight += 0.1
+		else if (songLength < 6 * 1e3) weight += 0.06
+		else if (songLength < 9 * 1e3) weight += 0.03
 	}
 	return weight.toFixed(2) * 100
 }
 
 const selectList = (list, info) => {
-	var result = list[0];
-	var lastweighting = 0;
-	var weighting = 0;
-	for (let index = 0; index < list.length; index++) {
-		weighting = calcWeight(list[index], info);
-		if (lastweighting < weighting) {
-			lastweighting = weighting
-			result = list[index]
-		}
-		//list[index].weight = calcWeight(list[index], info)
+	let result = list[0];
+	for (let index of list) {
+		index.weight = calcWeight(index, info);
+		if (result.weight < index.weight) result = index
 	}
-	// return selectArray(list)
 	return result;
-}
-
-const selectArray = array => {
-	var song = array[0]
-	for (let index = 1; index < array.length; index++) {
-		const nowSong = array[index];
-		if (song.weight < nowSong.weight) song = nowSong
-	}
-	return song
 }
 
 module.exports = (list, info) => {
@@ -85,3 +98,4 @@ module.exports = (list, info) => {
 };
 
 module.exports.ENABLE_FLAC = (process.env.ENABLE_FLAC || '').toLowerCase() === 'true'
+module.exports.ENABLE_WEIGHTINGSYSTEM = ENABLE_WEIGHTINGSYSTEM
